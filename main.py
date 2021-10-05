5818a3f7afc4abb1841678baaf4a0e6531305a77
emcody <gorczix@gmail.com>
2021-10-05 23:27:04 +0200

    Add Id

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ page = requests.get(URL)
 soup = bs(page.content, "html.parser")
 offers = soup.find_all("table", class_="fixed")
 
+print("{} offers found".format(len(offers)))
+
 for every in offers:
+    id = every.get("data-id")
     name = every.find("a", class_="marginright5").find("strong").get_text()
     price = every.find("p", class_="price").find("strong").get_text()
-    
-    print("\nName: " + name)
-    print("Price: " + price)
 
-
+    print("{} | {}  |  {}".format(id, price, name))
