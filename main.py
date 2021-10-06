dcff8ca9658c01e68595ae1b6e34251f161f9d0d
emcody <gorczix@gmail.com>
2021-10-06 22:59:31 +0200

    Scrap link and image

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ offers = soup.find_all("div", class_="offer-wrapper")
 print("{} offers found".format(len(offers)))
 
 for offer in offers:
-    code = offer.find("table", class_="fixed")
-    id = code.get("data-id")
-    name = offer.find("a", class_="marginright5").find("strong").get_text()
-    price = offer.find("p", class_="price").find("strong").get_text()
+    offer_table = offer.find("table", class_="fixed")
+    id = offer_table.get("data-id")
+    name = offer_table.find("a", class_="marginright5").find("strong").get_text()
+    price = offer_table.find("p", class_="price").find("strong").get_text()
     delivery = False
-    if offer.find("div", class_='olx-delivery-icon'):
+    if offer_table.find("div", class_='olx-delivery-icon'):
         delivery = True
+    link = offer_table.find("a", class_="thumb").get("href")
+    img = offer_table.find("img", class_="fleft").get("src")
     print("{} | {}  |  {}  |  {}".format(id, price, delivery, name))
+    print("{}".format(link))
+    print("{}".format(img))
