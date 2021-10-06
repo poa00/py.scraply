32d2eac9a9adfc6268e76c751e12be40e3a34c93
emcody <gorczix@gmail.com>
2021-10-06 22:47:59 +0200

    Fix first false element

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ URL = "https://www.olx.pl/oferty/q-reima-kombinezon-92/?search%5Border%5D=create
 
 page = requests.get(URL)
 soup = bs(page.content, "html.parser")
-offers = soup.find_all("table", class_="fixed")
+offers = soup.find_all("div", class_="offer-wrapper")
 
 print("{} offers found".format(len(offers)))
 
-for every in offers:
-    id = every.get("data-id")
-    name = every.find("a", class_="marginright5").find("strong").get_text()
-    price = every.find("p", class_="price").find("strong").get_text()
-
-    print("{} | {}  |  {}".format(id, price, name))
+for offer in offers:
+    id = offer.get("data-id")
+    name = offer.find("a", class_="marginright5").find("strong").get_text()
+    price = offer.find("p", class_="price").find("strong").get_text()
+    delivery = False
+    if offer.find("div", class_='olx-delivery-icon'):
+        delivery = True
+    print("{} | {}  |  {}  |  {}".format(id, price, delivery, name))
